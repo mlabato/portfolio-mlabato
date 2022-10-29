2f0d55981c159470619fd177e142c31168f9b7bf
mlabato <m_labato@hotmail.com>
2022-10-29 09:46:47 -0300

    Button styling changed

--- src/components/ux/Button.js
+++ src/components/ux/Button.js
@@ -1,6 +1,6 @@
 const Button = (props) => {
   return (
-    <button className="p-2  border-2 border-[#52796F] text-[#52796F] font-semibold rounded-lg hover:bg-[#52796F] hover:text-[#CAD2C5] ease-in-out duration-500">
+    <button className={"p-2  border-2 border-[#52796F] text-[#52796F] font-semibold rounded-lg hover:bg-[#52796F] hover:text-[#CAD2C5] ease-in-out duration-500 " + props.classes}>
       {props.text}
     </button>
   );
