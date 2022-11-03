bce43fda3945e5c26c7019be5e21aff011e76017
mlabato <m_labato@hotmail.com>
2022-11-02 22:58:21 -0300

    mobile fixes

--- src/components/sections/IntroSection.js
+++ src/components/sections/IntroSection.js
@@ -4,11 +4,11 @@ const IntroSection = () => {
 
       <div className=" notebook:text-[1.25rem] desktop:text-[1.5rem] full:text-[2rem] ml-[0.15rem]  ">Hi I'm</div>
 
-      <div className="absolute  top-[0.4rem]  desktop:top-[0.75rem]  font-extrabold text-[2.8rem] notebook:text-[4.62rem] desktop:text-[4.5rem] full:text-[8.2rem]  text-[#84A98C]">
+      <div className="absolute  top-[0.4rem]  desktop:top-[0.75rem]  font-extrabold text-[2.4rem] notebook:text-[4.62rem] desktop:text-[4.5rem] full:text-[8.2rem]  text-[#84A98C]">
         Martin Labato
       </div>
 
-      <div className="absolute  top-[3rem]  notebook:top-[4.5rem] desktop:top-[5.5rem] full:top-[8rem] font-extrabold text-[1.95rem] notebook:text-[3.4rem] desktop:text-[3.3rem] full:text-[6.1rem]  text-[#354F52]">
+      <div className="absolute  top-[3rem]  notebook:top-[4.5rem] desktop:top-[5.5rem] full:top-[8rem] font-extrabold text-[1.6rem] notebook:text-[3.4rem] desktop:text-[3.3rem] full:text-[6.1rem]  text-[#354F52]">
         I build experiences
       </div>
 
