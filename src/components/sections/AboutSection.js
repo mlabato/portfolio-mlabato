e7d9956bfabfcfbd0cb7776c2c55acfdeccb3a66
mlabato <m_labato@hotmail.com>
2023-02-16 10:36:21 -0300

    design modified

--- src/components/sections/AboutSection.js
+++ src/components/sections/AboutSection.js
@@ -24,35 +24,36 @@ const iteratedIcons = icons.map((icon, i) => {
 
 const AboutSection = () => {
   return (
-    <article id="about" className="mx-[2rem] notebook:mx-[15rem] desktop:mx-[20rem] full:mx-[25rem] relative top-[20rem] notebook:top-[30rem] desktop:top-[40rem] full:top-[60rem] animate-opacityChangeX">
+    <article id="about" className="mx-[10rem] pt-[15rem] animate-opacityChangeX ">
       <Title text={"about me"} />
-      <div className="flex notebook:flex-row my-[0.5rem] desktop:my-[1rem]">
-        <p className=" text-[0.8rem] notebook:w-2/3 desktop:text-[1rem] full:text-[1.6rem] ">
+      <div className="flex flex-row items-center my-[1rem] ">
+        <div className=" text-[1.2rem] w-2/3 mr-[2rem]">
           I’m not the typical web developer, because I’m 32 and since 2011 I
           have been working as legal counsellor, after obtaining both law and
           LLM degrees. However, in 2021 I pursued a wish I had for a long time:
           learn to code. I started a full stack developer bootcamp at Digital
           House and I discovered that I really liked it. That led on starting to
-          think in a big change in my career. 
-        </p>
-
-        <div
-          className="absolute border-2 border-[#354F52] mx-[4rem] rounded-xl my-auto
-          top-[21.5rem] right-[1.2rem] notebook:top-[1rem] notebook:bottom-0  notebook:my-auto notebook:-right-[9.5rem] desktop:-right-[6.5rem]    full:w-[17rem] full:h-[17rem]
-          w-[10rem] h-[10rem] notebook:w-[12rem] notebook:h-[12rem] desktop:w-[13rem] desktop:h-[13rem] 
-              desktop:hover:-right-[6.5rem] desktop:hover:top-[0.7rem] ease-in-out duration-300 "
+          think in a big change in my career.
+          <br />
+          <br />
+          Here are a few technologies I’ve been working with recently:
+          <div className="flex flex-row text-[1.2rem]  desktop:text-[2rem] full:text-[3rem]   my-[1rem] text-[#354F52]">
+            {iteratedIcons}
+          </div>
+        </div>
+        <img
+          src="/images/profile-image.jpeg"
+          alt=""
+          className="w-[20rem] rounded-xl z-50"
         />
-
-        <div className="absolute  bg-profile-picture bg-cover  rounded-xl
-        w-[10rem] h-[10rem] notebook:w-[12rem] notebook:h-[12rem] desktop:w-[13rem] desktop:h-[13rem] full:w-[17rem] full:h-[17rem]
-        top-[21rem] right-[6rem] notebook:top-0 notebook:bottom-0 notebook:my-auto notebook:-right-[5rem] desktop:-right-[2rem]   " />
-      </div>
-
-      <p className="text-[0.8rem] notebook:w-3/5 desktop:text-[1rem] full:text-[1.6rem]  ">
-        Here are a few technologies I’ve been working with recently:
-      </p>
-      <div className="flex flex-row text-[1.2rem]  desktop:text-[2rem] full:text-[3rem]   my-[1rem] text-[#354F52]">
-        {iteratedIcons}
+        <div className="relative z-10">
+          <div
+            className="absolute border-2 border-[#354F52] mx-[4rem] rounded-xl my-auto
+         -right-[5rem]    -top-[9rem]
+          w-[20rem] h-[20rem] 
+              desktop:hover:-right-[4.75rem] desktop:hover:-top-[9.25rem] ease-in-out duration-300 "
+          />
+        </div>
       </div>
     </article>
   );
--- src/components/sections/IntroSection.js
+++ src/components/sections/IntroSection.js
@@ -1,8 +1,8 @@
 const IntroSection = () => {
   return (
-    <section className="py-[15rem] mx-[10rem]  animate-opacityChangeY flex flex-row items-center">
+    <section className="pt-[15rem] mx-[10rem]  animate-opacityChangeY flex flex-row items-center">
       <article className="leading-tight">
-        <p className=" text-[1.5rem]   ">
+        <p className=" text-[1.5rem] mr-[2rem]  ">
           Hi I'm
         </p>
 
@@ -10,14 +10,14 @@ const IntroSection = () => {
           Martin Labato
         </p>
 
-        <p className="  text-[1rem] leading-relaxed ">
+        <p className="  text-[1.2rem] leading-normal ">
           I’m a web developer focused on developing stylish and accesible
           digital experiences. Currently, I’m strengthening my front end skills,
           but I can perform as a full stack developer.
         </p>
       </article>
 
-      <article><img src="/images/intro_img.svg" alt="" className="w-[80rem]"/></article>
+      <article><img src="/images/intro_img.svg" alt="" className="w-[90rem]"/></article>
     </section>
   );
 };
