26c89e8220b0516d19ec42c4ad11a26150b7630b
mlabato <m_labato@hotmail.com>
2023-02-16 11:44:48 -0300

    slider added

--- src/components/sections/WorksSection.js
+++ src/components/sections/WorksSection.js
@@ -3,45 +3,17 @@ import WorkCard from "../ux/WorkCard";
 
 const WorksSection = (props) => {
 
-const workList = [
-  {
-    name: "twitter app",
-    description: "A web app for visualizing personalized Twitter data. View your top retweeted and liked tweeted and detailed user information.",
-    tech:"Express + React + Twitter API",
-    website: "https://twitter-app-react.vercel.app/",
-    github: "https://github.com/mlabato/twitterapp-react",
-    image: "bg-twitter-app"
-  },
-  {
-    name: "outlet de mates",
-    description: "An e-commerce, which includes product browser, cart and e-payment methods. It also offers an admin dashboard with auth to manage stock and products. ",
-    tech: "Express + React + MySQL + MP",
-    image: "bg-odm-app"
-  },
-  {
-    name: "mr studio",
-    description: "Responsive landing page with contact form linked to Firebase database.",
-    tech: "React + Tailwind + Firebase",
-    image: "bg-landing-mr",
-    github: "https://github.com/mlabato/mr-studio",
-    website: "https://mr-studio.vercel.app/"
-  }
-]
+
  
 
 
   return(
-    <article id="works" className="mx-[3.5rem] notebook:mx-[15rem] desktop:mx-[20rem] full:mx-[25rem] my-[5rem] relative top-[34rem] notebook:top-[35rem] desktop:top-[40rem] full:top-[65rem] ">
+    <article id="works" className="mx-[10rem] pt-[10rem]  ">
         <Title text={"some things i have built"} />
         
-        {
-          workList.map((work, i) =>{
-            return(
-              <WorkCard name= {work.name} description={work.description} tech={work.tech} website={work.website} github={work.website} image={work.image} key={i}/>
-            )
-          })
-        }
-        
+  
+              <WorkCard />
+   
     </article>
   )
 };
--- src/components/ux/WorkCard.js
+++ src/components/ux/WorkCard.js
@@ -1,57 +1,71 @@
-import { FaGithub } from "react-icons/fa";
-import { FaRegEye } from "react-icons/fa";
+import { FaGithub, FaRegEye } from "react-icons/fa";
+import React, { useState, useEffect, useRef } from "react";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+
+let count = 0;
 
 const WorkCard = (props) => {
+  const workList = [
+    {
+      name: "twitter app",
+      description:
+        "A web app for visualizing personalized Twitter data. View your top retweeted and liked tweeted and detailed user information.",
+      tech: "Express + React + Twitter API",
+      website: "https://twitter-app-react.vercel.app/",
+      github: "https://github.com/mlabato/twitterapp-react",
+      image: "twitter-app.jpg",
+    },
+    {
+      name: "outlet de mates",
+      description:
+        "An e-commerce, which includes product browser, cart and e-payment methods. It also offers an admin dashboard with auth to manage stock and products. ",
+      tech: "Express + React + MySQL + MP",
+      image: "odm-app.jpg",
+    },
+    {
+      name: "mr studio",
+      description:
+        "Responsive landing page with contact form linked to Firebase database.",
+      tech: "React + Tailwind + Firebase",
+      image: "landing-mr.jpg",
+      github: "https://github.com/mlabato/mr-studio",
+      website: "https://mr-studio.vercel.app/",
+    },
+  ];
+
+  // //ARMADO DE HOOK
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  //FUNCIONAMIENTO BOTONES
+  const handleOnNextClick = () => {
+    count = (count + 1) % workList.length;
+    setCurrentIndex(count);
+  };
+  const handleOnPrevClick = () => {
+    const imagesLength = workList.length;
+    count = (currentIndex + imagesLength - 1) % imagesLength;
+    setCurrentIndex(count);
+  };
+
   return (
-    <article className="flex flex-row mb-[10rem] mt-[1rem] notebook:mt-[2rem] relative">
-      <div className="  notebook:w-[45%] mb-[4rem] notebook:mb-[3rem] desktop:mb-[7rem] full:mb-[15rem] notebook:text-justify ">
-        <div className="text-[#84A98C] text-[1.2rem]  notebook:text-[1.4rem] full:text-[2.3rem] font-semibold  notebook:ml-0 z-40">
-          {props.name}
-        </div>
-
-        <div className=" absolute notebook:w-[20rem] desktop:w-[22rem] full:w-[45rem] notebook:z-40 text-[0.8rem] z-40 ">
-          <p className=" notebook:bg-[#84A98C] text-[#354F52] w-[90%] notebook:w-full p-4 font-semibold notebook:font-normal   full:text-[1.7rem]   ">
-            {props.description}
-          </p>
-
-          <div className="absolute flex flex-row items-center notebook:items-start  justify-between notebook:justify-start notebook:flex-col notebook:relative
-          top-[9.5rem] notebook:top-0  px-1 desktop:p-0  notebook:w-full left-[0.2rem] notebook:left-0  
-          bg-[#84A98C] notebook:bg-transparent   notebook:p-0 ">
-            <p className="  notebook:ml-0 notebook:mt-[1rem] text-[#CAD2C5] notebook:text-[#354F52] text-[0.6rem] notebok:text-[1rem] full:text-[1.7rem] contrast-200 notebook:contrast-0">
-              {props.tech}
-            </p>
-
-            <div className=" flex flex-row justify-center my-[0.25rem] text-[#354F52] ml-[1rem] notebook:ml-0 ">
-              <a
-                href={props.website}
-                className="mr-[0.5rem] text-[1.25rem] full:text-[1.7rem]  "
-              >
-                <FaRegEye />
-              </a>{" "}
-              <a
-                href={props.github}
-                className="mr-[1rem] text-[1.25rem] full:text-[1.7rem] "
-              >
-                <FaGithub /> 
-              </a>
-            </div>
-          </div>
-        </div>
+    <div className="w-full group  select-none relative my-[1rem]  ">
+      <div className="">
+        <img
+          src= {"/images/" + workList[currentIndex].image}
+          className=" mx-auto w-full overflow-hidden opacity-80 "
+          alt=""
+        ></img>
       </div>
 
-      <div
-        className={
-          "absolute z-20 bg-cover rounded-lg desktop:rounded-xl opacity-20 notebook:opacity-100 h-[10rem]  notebook:h-[11rem] notebook:w-[21rem] desktop:h-[12rem] desktop:w-[24rem] full:h-[20rem] full:w-[40rem]    right-0 left-0 top-[2rem] mx-auto   notebook:-right-[15rem] notebook:top-[0.75rem] desktop:-right-[19rem] desktop:top-0 full:-right-[35rem]    " +
-          props.image
-        }
-      />
-      <div
-        className="absolute hidden notebook:block border-2 border-[#354F52] mx-[4rem] 
-      notebook:h-[11rem] notebook:w-[21rem]  desktop:h-[11rem]  desktop:w-[24rem]  full:h-[20rem] full:w-[40rem]
-      notebook:-right-[6.5rem] notebook:top-[1.75rem]   desktop:-right-[8rem] bigdesktop:-right-[4rem] full:-right-[9rem] desktop:top-[1.7rem] 
-      notebook:hover:-right-[6rem] notebook:hover:top-[1.2rem] desktop:hover:-right-[7.7rem] desktop:hover:top-[1.4rem] ease-in-out duration-300 rounded-xl my-auto"
-      />
-    </article>
+      <div className="absolute -inset-[4rem] flex p-4 justify-between items-center">
+        <button onClick={handleOnPrevClick} className="text-black  ">
+          <AiOutlineLeft size={40} className="text-[#354F52]  " />
+        </button>
+        <button onClick={handleOnNextClick} className="text-[#354F52] ">
+          <AiOutlineRight size={40} className="" />
+        </button>
+      </div>
+    </div>
   );
 };
 
